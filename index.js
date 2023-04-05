dd3aa254a4097b31a3298f8e1d5e2d7b768a68d1
Rohan Singh <65129632+rohansingh2023@users.noreply.github.com>
2023-04-06 00:37:00 +0530

    ssl certificate added

--- index.js
+++ index.js
@@ -2,9 +2,13 @@ import express from "express";
 import bodyParser from "body-parser";
 import mongoose from "mongoose";
 import cors from "cors";
-
+import fs from "fs";
+import https from "https";
 import postRoutes from "./routes/posts.js";
 import userRouter from "./routes/user.js";
+
+const key = fs.readFileSync("private.key");
+const cert = fs.readFileSync("certificate.crt");
 
 const app = express();
 
@@ -15,19 +19,40 @@ app.use(cors());
 app.use("/posts", postRoutes);
 app.use("/user", userRouter);
 
+app.get("/", (req, res) => {
+  res.send("API Working!!!");
+});
+
+const creds = {
+  key,
+  cert,
+};
+
 const CONNECTION_URL =
   "mongodb+srv://rohan:jhHhTuDPNp5o4avf@cluster0.ilfgxpf.mongodb.net/?retryWrites=true&w=majority";
 const PORT = process.env.PORT || 5000;
 
+// app.get(
+//   "/.well-known/pki-validation/6CF46F4E31415B276DEC5723363E6BC4.txt",
+//   (req, res) => {
+//     res.sendFile(
+//       "/home/ubuntu/blog-cc-api/6CF46F4E31415B276DEC5723363E6BC4.txt"
+//     );
+//   }
+// );
+
 mongoose
   .connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
-  .then(() =>
-    app.listen(PORT, () =>
-      console.log(`Server Running on Port: http://localhost:${PORT}`)
-    )
-  )
+  .then(() => console.log("MongoDB connected"))
   .catch((error) => console.log(`${error} did not connect`));
 
 mongoose.set("useFindAndModify", false);
+
+app.listen(PORT, () =>
+  console.log(`Server Running on Port: http://localhost:${PORT}`)
+);
+
+const httpsServer = https.createServer(creds, app);
+httpsServer.listen(8443);
 
 //jhHhTuDPNp5o4avf
